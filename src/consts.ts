0af7a5e1772c825a1c3b665835ce2e390c77e500
Cristiano-rj <cr.ferramentas.cia@gmail.com>
2026-05-11 15:44:27 -0300

    Atualizar o consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'CR Ferramentas & Cia | Manutenção e Precisão';
+export const SITE_DESCRIPTION = 'Blog especializado em manutenção mecânica industrial, serralheria e ferramentas de precisão no Rio de Janeiro.';
