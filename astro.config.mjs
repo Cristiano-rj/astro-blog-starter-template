f1c87887e8ae8bef513f0ba9b70308dace705e3d
Cristiano-rj <cr.ferramentas.cia@gmail.com>
2026-05-11 15:37:32 -0300

    Atualizar o astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,20 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: "https://crferramentascia.shop",
+  integrations: [mdx(), sitemap()],
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
